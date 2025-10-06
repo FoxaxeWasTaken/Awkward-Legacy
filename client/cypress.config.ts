a9256431be4a72a2c409e467ab2353d3532faa3d
Andreas LE TANTER <letanter.andreas@orange.fr>
2025-10-06 23:10:45 +0200

    fix: remove unused variables in cypress config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
     video: false,
     screenshotOnRunFailure: true,
     experimentalStudio: false,
